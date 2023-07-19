38967bcddf7a05e5168d2b1747063d1ed21ba163
Ecarcomo <emmanuelcarcomo@gmail.com>
2023-07-18 23:47:50 -0300

    new info
    update info poducts

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,54 +7,54 @@ export class AppService {
   getProducts(): VaperDto[] {
     return  [{
       id: 1,
-      name: "NikBar 1500 PUFF Cool Mint",
-      imageUrl: "https://nikbarofficial.com/images/CoolMint3.png",
-      description: "Vaper electronico nikbar 10ml E-liquid",
+      name: "NikBar 2500 PUFF Cool Mint",
+      imageUrl: "https://nikbarofficial.com/wp-content/uploads/2023/05/Cool-Mint-Nikbar-2500-Puffs.png",
+      description: "N2500 Puffs: Up to 2500\nE-liquid Capacity: 7,5ml\nNicotine Strength: 20mg & 50mg\nBattery Capacity: 950mAh\nCoil Type: COREX Heating Tech",
       price: 3500,
       offer_price:  2500,
       quantity: 10
     },
     {
       id: 2,
-      name: "NikBar 1500 PUFF Banana Ice",
-      imageUrl: "https://nikbarofficial.com/images/BananaIce7.png",
-      description: "Vaper electronico nikbar 10ml E-liquid",
+      name: "NikBar 4000 PUFF Blue Razz Ice",
+      imageUrl: "https://nikbarofficial.com/wp-content/uploads/2023/05/Blue-Razz-Ice-4000-Puffs.png",
+      description: "N4000 Puffs: Up to 4000\nE-liquid Capacity: 10ml\nNicotine Strength: 20mg & 50mg\nBattery Capacity: 400mAh (Rechargeable)\nCharging Port: Type-C\nCoil Type: COREX Heating Tech\nSize: 71x44x16mm",
       price: 3500,
       offer_price:  0,
       quantity: 10
     },
     {
       id: 3,
-      name: "NikBar 1500 PUFF Cool Mint",
-      imageUrl: "https://nikbarofficial.com/images/CoolMint3.png",
-      description: "Vaper electronico nikbar 10ml E-liquid",
+      name: "NikBar 2500 PUFF Cool Mint",
+      imageUrl: "https://nikbarofficial.com/wp-content/uploads/2023/05/Cool-Mint-Nikbar-2500-Puffs.png",
+      description: "N2500 Puffs: Up to 2500\nE-liquid Capacity: 7,5ml\nNicotine Strength: 20mg & 50mg\nBattery Capacity: 950mAh\nCoil Type: COREX Heating Tech",
       price: 3500,
       offer_price:  2500,
       quantity: 10
     },
     {
       id: 4,
-      name: "NikBar 1500 PUFF Banana Ice",
-      imageUrl: "https://nikbarofficial.com/images/BananaIce7.png",
-      description: "Vaper electronico nikbar 10ml E-liquid",
+      name: "NikBar 4000 PUFF Blue Razz Ice",
+      imageUrl: "https://nikbarofficial.com/wp-content/uploads/2023/05/Blue-Razz-Ice-4000-Puffs.png",
+      description: "N4000 Puffs: Up to 4000\nE-liquid Capacity: 10ml\nNicotine Strength: 20mg & 50mg\nBattery Capacity: 400mAh (Rechargeable)\nCharging Port: Type-C\nCoil Type: COREX Heating Tech\nSize: 71x44x16mm",
       price: 3500,
       offer_price:  0,
       quantity: 10
     },
     {
       id: 5,
-      name: "NikBar 1500 PUFF Cool Mint",
-      imageUrl: "https://nikbarofficial.com/images/CoolMint3.png",
-      description: "Vaper electronico nikbar 10ml E-liquid",
+      name: "NikBar 2500 PUFF Cool Mint",
+      imageUrl: "https://nikbarofficial.com/wp-content/uploads/2023/05/Cool-Mint-Nikbar-2500-Puffs.png",
+      description: "N2500 Puffs: Up to 2500\nE-liquid Capacity: 7,5ml\nNicotine Strength: 20mg & 50mg\nBattery Capacity: 950mAh\nCoil Type: COREX Heating Tech",
       price: 3500,
       offer_price:  2500,
       quantity: 10
     },
     {
       id: 6,
-      name: "NikBar 1500 PUFF Banana Ice",
-      imageUrl: "https://nikbarofficial.com/images/BananaIce7.png",
-      description: "Vaper electronico nikbar 10ml E-liquid",
+      name: "NikBar 4000 PUFF Blue Razz Ice",
+      imageUrl: "https://nikbarofficial.com/wp-content/uploads/2023/05/Blue-Razz-Ice-4000-Puffs.png",
+      description: "N4000 Puffs: Up to 4000\nE-liquid Capacity: 10ml\nNicotine Strength: 20mg & 50mg\nBattery Capacity: 400mAh (Rechargeable)\nCharging Port: Type-C\nCoil Type: COREX Heating Tech\nSize: 71x44x16mm",
       price: 3500,
       offer_price:  0,
       quantity: 10
